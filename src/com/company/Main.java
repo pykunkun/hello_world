c08d521fb08ef1285b983a6f7280bc6f19481db7
Mi <358581726@qq.com>
2018-10-15 15:19:11 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1234，弄好");
+    }
+}
